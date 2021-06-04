b81fa066ef8dfae0dbf3cfb44f066a039c42f124
Gyanesh Satyawali <70893994+Coder2011-ALT@users.noreply.github.com>
2021-06-04 13:00:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function textGrow() {
+    document.getElementById("lonely").style.fontSize = "70px";
+}
+
+function textShrink() {
+    document.getElementById("lonely").style.fontSize = "50px";
+}
